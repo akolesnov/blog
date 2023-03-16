519ecf81bb481ebbc73235e19ed7c3927bc00bfc
akolesnov <a.kolesnov@yandex.ru>
2023-03-17 00:11:19 +0300

    Created form for posts adding

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -5,8 +5,12 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Form\PostType;
 use App\Repository\PostRepository;
+use Cocur\Slugify\Slugify;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,9 +18,12 @@ class PostsController extends AbstractController
 {
     private $postRepository;
 
-    public function __construct(PostRepository $postRepository)
+    private $entityManager;
+
+    public function __construct(PostRepository $postRepository, EntityManagerInterface $entityManager)
     {
         $this->postRepository = $postRepository;
+        $this->entityManager = $entityManager;
     }
 
     #[Route('/posts', name: 'blog_posts')]
@@ -29,11 +36,32 @@ class PostsController extends AbstractController
         ]);
     }
 
-    #[Route('posts/{slug}', name: 'blog_show')]
+    #[Route('/posts/new', name: 'new_blog_post')]
+    public function addPost(Request $request, Slugify $slugify, ): Response
+    {
+        $post = new Post();
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post->setSlug($slugify->slugify($post->getTitle()));
+            $post->setCreatedAt(new \DateTime());
+
+            $this->entityManager->persist($post);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('blog_posts');
+        }
+        return $this->render('posts/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/posts/{slug}', name: 'blog_show')]
     public function post(Post $post)
     {
         return $this->render('posts/show.html.twig', [
-           'post' => $post
+            'post' => $post
         ]);
     }
 }
