132b6f26d0ad5655922fe8559ff9263913c16813
akolesnov <a.kolesnov@yandex.ru>
2023-03-16 18:17:00 +0300

    Show posts in templates

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -1,28 +1,39 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostsController extends AbstractController
 {
-    #[Route('/posts', name: 'app_posts')]
-    public function index(): Response
+    private $postRepository;
+
+    public function __construct(PostRepository $postRepository)
     {
-        $posts = [
-            'post_1' => [
-                'title' => 'Заголовок первого поста',
-                'body' => 'Тело первого поста'
-            ],
-            'post_2' => [
-                'title' => 'Заголовок второго поста',
-                'body' => 'Тело второго поста'
-            ]
-        ];
+        $this->postRepository = $postRepository;
+    }
+
+    #[Route('/posts', name: 'blog_posts')]
+    public function posts(): Response
+    {
+        $posts = $this->postRepository->findAll();
+
         return $this->render('posts/index.html.twig', [
             'posts' => $posts,
+        ]);
+    }
+
+    #[Route('posts/{slug}', name: 'blog_show')]
+    public function post(Post $post)
+    {
+        return $this->render('posts/show.html.twig', [
+           'post' => $post
         ]);
     }
 }
